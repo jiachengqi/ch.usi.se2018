4e088a0398d520676edb9ddd4bb35e977d7f9639
Valerio <valerio.terragni@usi.ch>
2018-10-16 11:05:23 +0200

    add a bug

--- src/main/java/ch.usi.simplejavacalculator/MathOperations.java
+++ src/main/java/ch.usi.simplejavacalculator/MathOperations.java
@@ -6,7 +6,7 @@ public class MathOperations {
 
 
     protected static Double add(Double num1, Double num2) {
-        return num1+num2;
+        return num1*num2;
     }
 
     protected static Double minus(Double num1, Double num2) {
